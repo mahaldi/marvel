ab5ea275e0b18355d459af9323e03d25a11fb951
mahaldiWS_ <aldi@umroh.com>
2020-04-24 02:01:05 +0700

    404 page

---
+++ src/views/pages/NotFoundPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+	return (
+		<React.Fragment>
+			<div className="404 hero-body">
+				<div className="container has-text-centered">
+					<h1 className="title is-1">404</h1>
+					<h2 className="is-4">The page you looking for doesn't exist</h2>
+				</div>
+			</div>
+		</React.Fragment>
+	)
+}
+
+export default NotFoundPage
