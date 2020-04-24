77c879e267f775c164cac038987c6e55ccde9f79
mahaldiWS_ <aldi@umroh.com>
2020-04-25 01:35:42 +0700

    input event

--- src/views/elements/input/index.jsx
+++ src/views/elements/input/index.jsx
@@ -4,23 +4,41 @@ class Input extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			value : ''
+			value : '',
+			isFocus: false
 		}
 	}
 	static defaultProps = {
-		placeholder : 'Search Spider'
+		placeholder : 'Search spider'
 	}
 	_onChange = (e) => {
 		this.setState({
 			value: e.target.value
 		})
+		if(this.props.onSearch)
+			this.props.onSearch(e.target.value)
+	}
+	_onFocus = (e) => {
+		let isFocus = !this.state.isFocus
+
+		this.setState({
+			isFocus
+		})
+		this.props.onFocus(isFocus)
 	}
 	render() {
 		let { value } = this.state
 		let { placeholder } = this.props
 		return (
 			<React.Fragment>
-				<input type="text" className="mws-input" value={value} placeholder={placeholder} onChange={this._onChange}/>
+				<input
+					type="text"
+					className="mws-input"
+					value={value}
+					placeholder={placeholder}
+					onFocus={this._onFocus}
+					onBlur={this._onFocus}
+					onChange={this._onChange}/>
 			</React.Fragment>
 		)
 	}
