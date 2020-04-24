1571144cb13ed48f5ba0022858802e894c8978f7
mahaldiWS_ <aldi@umroh.com>
2020-04-25 01:34:27 +0700

    header component refactor

--- src/views/components/header/index.jsx
+++ src/views/components/header/index.jsx
@@ -2,49 +2,62 @@ import React from "react";
 import './style.scss';
 import Logo from '../../../assets/static/marvel.svg'
 import SearchInput from '../../components/searchInput'
+import { Link } from 'react-router-dom'
 
-const Header = () => {
-	return (
-		<React.Fragment>
-			<nav className="navbar is-fixed-top has-background-white navbar-mws">
-					<div className="container">
-						<div className="navbar-brand">
-							<a className="navbar-item" href="/">
-								<img src={Logo} alt="Logo" />
-							</a>
-							{/* <span className="navbar-burger burger" data-target="navbarMenuHeroB">
-								<span></span>
-								<span></span>
-								<span></span>
-							</span> */}
-						</div>
-						<div id="navbarMenuHeroB" className="navbar-menu">
-							<div className="navbar-start middle">
-								<div className="navbar-item">
-									<SearchInput />
-                </div>
+class Header extends React.Component {
+	lintenRouteChanging = () => {
+		this.unlisten = this.props.history.listen((location, action) => {
+      // console.log("on route change", location);
+      // console.log("action", action);
+    });
+	}
+	destroyLintening = () => {
+		this.unlisten();
+	}
+	componentDidMount() {
+		this.lintenRouteChanging()
+	}
+	componentWillUnmount() {
+		this.destroyLintening();
+	}
+	render() {
+		return (
+			<React.Fragment>
+				<nav className="navbar is-fixed-top has-background-white navbar-mws">
+						<div className="container">
+							<div className="navbar-brand">
+								<Link className="navbar-item" to="/">
+									<img src={Logo} alt="Logo" />
+								</Link>
 							</div>
-							<div className="navbar-end">
-								<div className="navbar-item has-dropdown is-hoverable">
-									<div className="navbar-link">
-										Theme
+							<div id="navbarMenuHeroB" className="navbar-menu">
+								<div className="navbar-start middle">
+									<div className="navbar-item">
+										<SearchInput />
 									</div>
-									<div className="navbar-dropdown is-boxed">
-										<div className="navbar-item">
-											Dark Theme
+								</div>
+								<div className="navbar-end">
+									<div className="navbar-item has-dropdown is-hoverable">
+										<div className="navbar-link">
+											Theme
 										</div>
-										<div className="navbar-item">
-											Light Theme
-										</div>
+										<div className="navbar-dropdown is-boxed">
+											<div className="navbar-item">
+												Dark Theme
+											</div>
+											<div className="navbar-item">
+												Light Theme
+											</div>
 
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</nav>
-		</React.Fragment>
-	)
+					</nav>
+			</React.Fragment>
+		)
+	}
 }
 
 export default Header;
