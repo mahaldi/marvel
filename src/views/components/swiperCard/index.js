72322aa489e0e4404151aafb5b71d4970b96520a
mahaldiWS_ <aldi@umroh.com>
2020-04-28 12:33:55 +0700

    swiper card error state

--- src/views/components/swiperCard/index.js
+++ src/views/components/swiperCard/index.js
@@ -9,7 +9,7 @@ import ErrorContent from '../ErrorContent'
 const SwiperCard = (props) => {
 	const params = {
 		slidesPerView: 'auto',
-		spaceBetween: 30,
+		spaceBetween:  16,
 		slidesOffsetBefore: 16,
 		navigation: {
 			nextEl: '.swiper-button-next',
@@ -18,7 +18,7 @@ const SwiperCard = (props) => {
 	}
 
 	let { data, loading, error, type } = props
-	if( error ) return <ErrorContent />
+	if( error ) return <ErrorContent error={error.data}/>
 	if( loading ) return <MiniLoader />
 	return (
 		<div className="mws-swiper-card">
@@ -27,7 +27,7 @@ const SwiperCard = (props) => {
 					<p className="title">{ type }</p>
 				</div>
 				<div className="right">
-					<Link to={ '/' + type }> Lihat Semua</Link>
+					<Link to={ '/' + type.toLowerCase() }> Lihat Semua</Link>
 				</div>
 			</div>
 			<Swiper {...params}>
