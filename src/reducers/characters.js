107ffcd6234fb5844de80c2360c20b45365862cc
mahaldiWS_ <aldi@umroh.com>
2020-04-28 11:40:40 +0700

    fetch characters state

--- src/reducers/characters.js
+++ src/reducers/characters.js
@@ -1,4 +1,4 @@
-import { FETCH_CHARACTER, FETCH_CHARACTERS } from '../actions/types'
+import { FETCH_CHARACTER, FETCH_CHARACTERS, FETCH_CHARACTERS_BEGIN, FETCH_CHARACTERS_FAILURE } from '../actions/types'
 import initialState from './initialState'
 
 export const characterList = (state = initialState.characters, action) => {
@@ -13,6 +13,18 @@ export const character = (state = initialState.character, action) => {
 	switch (action.type) {
 		case FETCH_CHARACTER:
 			return action.payload;
+		default:
+			return state
+	}
+}
+export default (state = initialState.characters, action) => {
+	switch (action.type) {
+		case FETCH_CHARACTERS_BEGIN:
+			return { ...state, loading: true }
+		case FETCH_CHARACTERS:
+			return { ...state, loading: false, items: action.payload, error: false }
+		case FETCH_CHARACTERS_FAILURE:
+			return { ...state, loading: false, error: action.payload }
 		default:
 			return state
 	}
