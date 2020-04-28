00583e7f3a9d32c0275fdf9dc49ea555024952cf
mahaldiWS_ <aldi@umroh.com>
2020-04-28 11:42:37 +0700

    footer component

---
+++ src/views/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.scss'
+
+const Footer = () => {
+	return (
+		<React.Fragment>
+			<div className="mws-footer">
+				<div>Github Link : <a href="https://github.com/mahaldi/marvel" target="_blank" rel="noopener noreferrer">https://github.com/mahaldi/marvel</a></div>
+				<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" rel="noopener noreferrer" title="Flaticon" target="_blank">www.flaticon.com</a></div>
+			</div>
+		</React.Fragment>
+	)
+}
+
+export default Footer
